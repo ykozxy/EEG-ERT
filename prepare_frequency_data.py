fdbc1c6f79a21708ea83ec3c01434d4f08bac995
zhang-yubo <52626825+zhang-yubo@users.noreply.github.com>
2024-03-04 21:37:36 -0800

    Added y train to freq prep

--- prepare_frequency_data.py
+++ prepare_frequency_data.py
@@ -16,13 +16,19 @@ person_test = np.load("data/person_test.npy")
 print("filtering data...")
 X_train_valid_filtered_30_100 = [mne.filter.filter_data(trial, sfreq, 30, 100, verbose=False) for trial in X_train_valid]
 X_train_valid_filtered_0_30 = [mne.filter.filter_data(trial, sfreq, 0, 30, verbose=False) for trial in X_train_valid]
+X_test_filtered_30_100 = [mne.filter.filter_data(trial, sfreq, 30, 100, verbose=False) for trial in X_test]
+X_test_filtered_0_30 = [mne.filter.filter_data(trial, sfreq, 0, 30, verbose=False) for trial in X_test]
 
 print("concatenating...")
 X_train_valid_augmented = np.concatenate([X_train_valid, X_train_valid_filtered_30_100, X_train_valid_filtered_0_30])
+y_train_valid_augmented = np.concatenate([y_train_valid] * 3)
 person_train_valid_augmented = np.concatenate([person_train_valid] * 3)
 
 print("saving...")
 np.save("data/generated/frequency/X_train_valid_augmented.npy", X_train_valid_augmented)
 np.save("data/generated/frequency/person_train_valid_augmented.npy", person_train_valid_augmented)
+np.save("data/generated/frequency/X_test_filtered_30_100.npy", X_test_filtered_30_100)
+np.save("data/generated/frequency/X_test_filtered_0_30.npy", X_test_filtered_0_30)
+np.save("data/generated/frequency/y_train_valid_augmented.npy", y_train_valid_augmented)
 
 print("done!")
