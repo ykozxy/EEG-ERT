07e466028073c84e2e0c467f0f4fc7d6603485ea
Xuyang Zhou <nick.zhouxy@gmail.com>
2024-03-05 18:03:44 -0800

    One-hot encoding and label smoothing of y

--- utils/dataset.py
+++ utils/dataset.py
@@ -4,12 +4,24 @@ import numpy as np
 class EEGDataset:
     """ PyTorch Dataset for EEG data """
 
-    def __init__(self, X_file: str, y_file: str, shuffle: bool = True):
-        self.X = np.load(X_file)
-        self.y = np.load(y_file)
+    def __init__(self, X: np.ndarray, y: np.ndarray, shuffle: bool = True, label_smoothing: bool = True):
+        self.X = X
+        self.y = y
 
+        # Check shape
         if self.X.shape[0] != self.y.shape[0]:
             raise ValueError("X and y must have the same length")
+
+        # Encode y
+        self.y -= self.y.min()
+        self.num_class = self.y.max() + 1
+        if label_smoothing:
+            # True class probability: 0.9
+            # Other class probability: 0.1 / (num_class - 1)
+            self.y = np.eye(self.num_class)[self.y] * 0.9
+            self.y[self.y == 0] = 0.1 / (self.num_class - 1)
+        else:
+            self.y = np.eye(self.num_class)[self.y]
 
         if shuffle:
             idx = np.random.permutation(self.X.shape[0])
@@ -27,7 +39,8 @@ if __name__ == '__main__':
     # Testing
     X_file = '../data/X_train_valid.npy'
     y_file = '../data/y_train_valid.npy'
-    dataset = EEGDataset(X_file, y_file)
+    dataset = EEGDataset(np.load(X_file), np.load(y_file))
     print(len(dataset))
     print(dataset[0][0].shape)
-    print(dataset[0][1])
+    print(dataset[0][1].shape)
+    print(dataset[0])
