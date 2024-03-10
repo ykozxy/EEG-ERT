16ac2de744a625c48f64ca3eb361323005a3efc6
zhang-yubo <52626825+zhang-yubo@users.noreply.github.com>
2024-03-09 19:41:07 -0800

    RNN 40% acc and Data Augmentation

--- prepare_frequency_data.py
+++ prepare_frequency_data.py
@@ -1,10 +1,13 @@
 import os
 import numpy as np
 import mne
+import braindecode.augmentation as Augmentations
 
 os.makedirs('data/generated/frequency', exist_ok=True)
 
 sfreq = 250
+
+print(f"Loading data...")
 
 X_test = np.load("data/X_test.npy")
 y_test = np.load("data/y_test.npy")
@@ -18,28 +21,35 @@ y_train = y_train_valid[:1777]
 person_train = person_train_valid[:1777]
 
 X_train_aug = [X_train]
+concat_times = len(X_train_aug)
 
-print("filtering data...")
-X_train_filtered_30_100 = [mne.filter.filter_data(trial, sfreq, 30, 100, verbose=False) for trial in X_train]
-X_train_filtered_0_30 = [mne.filter.filter_data(trial, sfreq, 0, 30, verbose=False) for trial in X_train]
-X_test_filtered_30_100 = [mne.filter.filter_data(trial, sfreq, 30, 100, verbose=False) for trial in X_test]
-X_test_filtered_0_30 = [mne.filter.filter_data(trial, sfreq, 0, 30, verbose=False) for trial in X_test]
+# print("filtering data...")
+# X_train_filtered_30_100 = [mne.filter.filter_data(trial, sfreq, 30, 100, verbose=False) for trial in X_train]
+# X_train_filtered_0_30 = [mne.filter.filter_data(trial, sfreq, 0, 30, verbose=False) for trial in X_train]
+# X_test_filtered_30_100 = [mne.filter.filter_data(trial, sfreq, 30, 100, verbose=False) for trial in X_test]
+# X_test_filtered_0_30 = [mne.filter.filter_data(trial, sfreq, 0, 30, verbose=False) for trial in X_test]
 
-surrogate_times = 5
+surrogate_times = 4
 for i in range(surrogate_times):
-    print(f"transforming data ({i})...")
-    fft_data = np.fft.fft(X_train, axis=-1)
-    random_phase = np.exp(1j * np.random.uniform(0, 2 * np.pi, size=fft_data.shape))
-    fft_data_randomized = fft_data * random_phase
-    data_randomized = np.fft.ifft(fft_data_randomized, axis=-1)
-    data_randomized = np.real(data_randomized)
-    X_train_aug.append(data_randomized)
+    print(f"generating ft surrogate ({i})...")
+    ftsurrogate = Augmentations.FTSurrogate(probability=0.5, channel_indep=False)
+    sr = np.array([ftsurrogate(trial) for trial in X_train])
+    X_train_aug.append(sr)
+    concat_times += 1
+
+stm_times = 0
+for i in range(stm_times):
+    print(f"generating smooth time masks ({i})...")
+    smooth = Augmentations.SmoothTimeMask(probability=1, mask_len_samples=140)
+    stm = np.array([smooth(trial) for trial in X_train])
+    X_train_aug.append(stm)
+    concat_times += 1
 
 print("concatenating...")
 # X_train_augmented = np.concatenate([X_train, X_train_filtered_30_100, X_train_filtered_0_30])
 X_train_augmented = np.concatenate(X_train_aug)
-y_train_augmented = np.concatenate([y_train] * (1 + surrogate_times))
-person_train_augmented = np.concatenate([person_train] * (1 + surrogate_times))
+y_train_augmented = np.concatenate([y_train] * (concat_times))
+person_train_augmented = np.concatenate([person_train] * (concat_times))
 
 print("saving...")
 np.save("data/generated/frequency/X_train_augmented.npy", X_train_augmented)
